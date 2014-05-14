a47452ba9f66a5773dc9e7bbe0cd047b21e54799
quattromani <max@maxquattromani.com>
2014-05-14 10:21:37 -0600

    repair font files

--- js/build/production.js
+++ js/build/production.js
@@ -80,8 +80,6 @@ $('.progress').click(function() {
     $(this).addClass('js-active');
     $('.progress.js-active i').progressButton();
 });
-
-$('.progress.js-active i').progressButton();
 var smallBreakPoint = 640;
 var mediumBreakPoint = 768;
 (function($) {
--- js/buttons.js
+++ js/buttons.js
@@ -6,5 +6,3 @@ $('.progress').click(function() {
     $(this).addClass('js-active');
     $('.progress.js-active i').progressButton();
 });
-
-$('.progress.js-active i').progressButton();
