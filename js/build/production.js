354d3d7a0d3f6c229b35f8be0bf7ad134d1f9914
quattromani <max@maxquattromani.com>
2014-06-02 13:25:59 -0600

    rewrite progress buttons function

--- js/build/production.js
+++ js/build/production.js
@@ -76,11 +76,25 @@ $('.fonts').each(function(){
     Progress Buttons -- Version: 0.2.1 - Updated: 5/14/2014
    ========================================================================== */
 
-$('.progress').click(function() {
-  $(this).attr("disabled", true);
-  $(this).addClass('js-active');
-  $('.progress.js-active i').progressButton();
-});
+// $('.progress').click(function() {
+//   $(this).attr("disabled", true);
+//   $(this).addClass('js-active');
+//   $('.progress.js-active i').progressButton();
+// });
+
+(function($) {
+
+  $.fn.progressButton = function() {
+    $('.progression').click(function() {
+      $(this).attr("disabled", true);
+      $(this).addClass('js-active');
+    });
+  }
+
+}(jQuery));
+
+$('.progression').progressButton();
+
 var smallBreakPoint = 640;
 var mediumBreakPoint = 768;
 (function($) {
--- js/buttons.js
+++ js/buttons.js
@@ -2,8 +2,21 @@
     Progress Buttons -- Version: 0.2.1 - Updated: 5/14/2014
    ========================================================================== */
 
-$('.progress').click(function() {
-  $(this).attr("disabled", true);
-  $(this).addClass('js-active');
-  $('.progress.js-active i').progressButton();
-});
+// $('.progress').click(function() {
+//   $(this).attr("disabled", true);
+//   $(this).addClass('js-active');
+//   $('.progress.js-active i').progressButton();
+// });
+
+(function($) {
+
+  $.fn.progressButton = function() {
+    $('.progression').click(function() {
+      $(this).attr("disabled", true);
+      $(this).addClass('js-active');
+    });
+  }
+
+}(jQuery));
+
+$('.progression').progressButton();
