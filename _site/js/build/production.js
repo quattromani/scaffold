7a7880b087d2d0a58d7edf95d411fb556f5f0c2b
quattromani <max@maxquattromani.com>
2014-07-07 08:16:12 -0600

    begin adding drag and drop

--- _site/js/build/production.js
+++ _site/js/build/production.js
@@ -95,6 +95,24 @@ $('.fonts').each(function(){
 
 $('.progress').progressButton();
 
+/* ==========================================================================
+    Drop -- Version: 0.2.1 - Updated: 7/8/2014
+   ========================================================================== */
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
 var smallBreakPoint = 640;
 var mediumBreakPoint = 768;
 (function($) {
---
+++ js/drop.js
@@ -0,0 +1,17 @@
+/* ==========================================================================
+    Drop -- Version: 0.2.1 - Updated: 7/8/2014
+   ========================================================================== */
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
