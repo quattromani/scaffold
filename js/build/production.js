d2cefeee6d25c66e9ef879c06cf36753ebce4763
quattromani <max@maxquattromani.com>
2014-07-11 22:01:58 -0600

    add carousel component

--- js/build/production.js
+++ js/build/production.js
@@ -96,6 +96,45 @@ $('.fonts').each(function(){
 $('.progress').progressButton();
 
 /* ==========================================================================
+    Slick Carousel -- Version: 0.2.1 - Updated: 5/14/2014
+    ========================================================================== */
+
+$('.carousel').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
+
+/* ==========================================================================
     Drop -- Version: 0.2.1 - Updated: 7/8/2014
    ========================================================================== */
 
