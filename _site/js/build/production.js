171a141a3dee44eb8bde045a15a605c62b0e0232
quattromani <max@maxquattromani.com>
2014-08-27 16:51:54 -0600

    compliance-card

--- _site/js/build/production.js
+++ _site/js/build/production.js
@@ -1,3 +1,5 @@
+var smallBreakPoint = 640;
+var mediumBreakPoint = 768;
 /* ==========================================================================
     Prototype Tools -- Version: 1.9.0.2 - Updated: 1/20/2014
    ========================================================================== */
@@ -94,43 +96,41 @@ $('.progress').progressButton();
     Slick Carousel -- Version: 0.2.1 - Updated: 5/14/2014
     ========================================================================== */
 
-$('.carousel').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-  ]
-});
+// $('.carousel').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 1000,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         centerMode: true,
+//         centerPadding: '40px',
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       }
+//     }
+//   ]
+// });
 
-var smallBreakPoint = 640;
-var mediumBreakPoint = 768;
 (function($) {
 
 	$.fn.jumpTo = function() {
