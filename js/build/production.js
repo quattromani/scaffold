527ebc7bbf43a2a57fda656ac36b50f926f43292
quattromani <max@maxquattromani.com>
2014-10-15 22:15:54 -0600

    added logo and changed some colors

--- js/build/production.js
+++ js/build/production.js
@@ -329,12 +329,27 @@ $('input, textarea').placeholder();
     sandbox -- Version: 0.2.2 - Updated: 7/26/2014
     ========================================================================== */
 
+    // $(function() {
+    //   $(".radio.reveally").click(function() {
+    //     var cat = $(this).val();
+
+    //     $(".hidden").hide();
+    //     $(".category" + cat).fadeIn(500);
+    //   });
+    // });
+
     $(function() {
-      $("input[type='radio']").click(function() {
+      $(".radio.reveally").change(function() {
         var cat = $(this).val();
 
-        $(".hidden").hide();
-        $(".category" + cat).fadeIn(500);
+        if(this.checked) {
+          $(".hidden").hide();
+          $(".category" + cat).slideDown(500);
+        }
+      });
+
+      $('button[type=reset]').click(function () {
+        $(".hidden").slideUp(500);
       });
     });
 
