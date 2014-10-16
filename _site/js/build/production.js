5d424a7c5974c487b66abb41e1bb8430b7663581
quattromani <max@maxquattromani.com>
2014-10-15 22:29:52 -0600

    temporarily turned off .getYear

--- _site/js/build/production.js
+++ _site/js/build/production.js
@@ -64,16 +64,16 @@ $(function() {
 });
 
 // Set year
-(function($) {
+// (function($) {
 
-  $.fn.getYear = function() {
-    var d = new Date();
-    var x = document.getElementById("year");
-    x.innerHTML=d.getFullYear();
-  }
+//   $.fn.getYear = function() {
+//     var d = new Date();
+//     var x = document.getElementById("year");
+//     x.innerHTML=d.getFullYear();
+//   }
 
-}(jQuery));
-$('.getYear').getYear();
+// }(jQuery));
+// $('.getYear').getYear();
 
 // Open all external links in a new window
 $('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
