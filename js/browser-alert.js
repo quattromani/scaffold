8267f8e2a8dca3bdcef9e33a36bc805fa85142a4
quattromani <max@maxquattromani.com>
2014-12-17 15:03:20 -0700

    browser-alert

---
+++ js/browser-alert.js
@@ -0,0 +1,19 @@
+/* ==========================================================================
+    browser-alert -- Version: 0.1.1 - Updated: 12/17/2014
+    ========================================================================== */
+
+(function($) {
+
+  $.fn.browserAlert = function() {
+    $('.browser-alert').each(function() {
+
+      var alert = jQuery(this);
+
+
+
+    });
+  }
+
+}(jQuery));
+
+$('.browser-alert').browserAlert();
